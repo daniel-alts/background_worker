68c29cb91596260922ee3229a70a61358d2a5c54
Adesoji Daniel <sojidaniels@gmail.com>
2024-08-03 18:55:44 +0100

    update redis url

--- workersetup.js
+++ workersetup.js
@@ -1,12 +1,13 @@
 const Bull = require('bull');
 
 const Worker = () => {
+  const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379'
   const redisHost = process.env.REDIS_HOST || '127.0.0.1';
   const redisPort = process.env.REDIS_PORT || 6379;
   const queueName = 'background_jobs';
 
   // A queue for the jobs scheduled based on a routine without any external requests
-  const backgroundJob = new Bull(queueName, { redis: { port: redisPort, host: redisHost } });
+  const backgroundJob = new Bull(queueName, { redis: redisUrl  });
 
   const start = () => {
     backgroundJob.process(function (job, done) {
